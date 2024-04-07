6f676c7d4eafd6d7ef8c426c01e4fc56f1d5fd8b
Yuliia7777 <yuliiakorobkova1@gmail.com>
2024-04-07 17:06:14 +0200

    hw11.1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,6 +2,11 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const renderData = data => {
   const gallery = document.getElementById('gallery');
